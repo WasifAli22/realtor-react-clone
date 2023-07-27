837c43947469b64da4a245c1acf5c420db1fd334
Wasif Shahid <wasifaliwasif22@gmail.com>
2023-07-27 21:13:33 +0500

    Added Forgot password functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import OAuth from '../components/OAuth'
+import { toast } from 'react-toastify'
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth'
 export default function ForgotPassword() {
 
   const [email, setEmail] = useState("")
 
   function onChange(e) {
     setEmail(e.target.value)
+  }
+  async function onSubmit(e) {
+    e.preventDefault()
+    try {
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email)
+      toast.success("Email was sent")
+    } catch (error) {
+      toast.error("Couldn't send reset password")
+    }
   }
   return (
     <section>
@@ -17,9 +29,9 @@ export default function ForgotPassword() {
             alt="" className='w-full rounded-2xl' />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input type="email" id='email' value={email} onChange={onChange} placeholder='Email address' className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' />
-  
+
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className='mb-6'>Don't have a account?
                 <Link to="/sign-up" className='ml-1 text-red-600 hover:text-red-700 transition duration-200 ease-in-out'>Register</Link>
@@ -30,7 +42,7 @@ export default function ForgotPassword() {
             <div className="my-4 before:border-t flex before:flex-1 items-center before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
               <p className='text-center font-semibold mx-4'>OR</p>
             </div>
-            <OAuth/>
+            <OAuth />
           </form>
 
         </div>
