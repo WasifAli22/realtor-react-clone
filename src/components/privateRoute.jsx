863542b874534aac16e54ca1e1a812e60d073e17
Wasif Shahid <wasifaliwasif22@gmail.com>
2023-07-28 16:55:19 +0500

    Add the Edit functionality in the profile page. And also added comments in the profile page and private route page

--- src/components/privateRoute.jsx
+++ src/components/privateRoute.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import { Outlet, Navigate } from 'react-router'
+import React from 'react';
+import { Outlet, Navigate } from 'react-router';
 import { useAuthStatus } from '../hooks/useAuthStatus';
+
 export default function PrivateRoute() {
-    const { loggedIn, checkingStatus } = useAuthStatus()
+    // Use the 'useAuthStatus' custom hook to get the authentication status and checking status
+    const { loggedIn, checkingStatus } = useAuthStatus();
+
+    // Check if the authentication status is still being checked
     if (checkingStatus) {
-        return <h3>Loading...</h3>
+        // If checking is in progress, display a simple loading message
+        return <h3>Loading...</h3>;
     }
-    return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />
+
+    // If checking is completed, check if the user is logged in
+    return loggedIn ? (
+        // If the user is logged in, render the nested routes defined by 'Outlet'
+        <Outlet />
+    ) : (
+        // If the user is not logged in, redirect them to the sign-in page
+        <Navigate to="/sign-in" />
+    );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,19 +1,65 @@
-import { getAuth } from 'firebase/auth'
-import React, { useState } from 'react'
+import { getAuth, updateProfile } from 'firebase/auth';
+import { doc, updateDoc } from 'firebase/firestore';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
+import { toast } from 'react-toastify';
+import { db } from '../firebase';
 
 export default function Profile() {
-  const auth = getAuth()
+  // Get the authentication instance and the navigation hook
+  const auth = getAuth();
   const navigate = useNavigate();
+
+  // State variables to handle user profile details and edit mode
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFromData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
-  })
-  const { name, email } = formData
+  });
+
+  // Destructure user profile details from the formData object
+  const { name, email } = formData;
+
+  // Function to handle user logout
   function onLogout() {
-    auth.signOut()
-    navigate("/")
+    auth.signOut();
+    navigate("/");
   }
+
+  // Function to handle changes in the input fields
+  function onChange(e) {
+    setFromData((prevstate) => ({
+      ...prevstate,
+      [e.target.id]: e.target.value,
+    }));
+  }
+
+  // Function to submit the profile changes to Firebase
+  async function onSubmit() {
+    try {
+      // Check if the display name has been changed
+      if (auth.currentUser.displayName !== name) {
+        // Update the display name in Firebase authentication
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // Update the name in the Firestore document
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+
+      // Show success toast message
+      toast.success("Profile detail updated");
+    } catch (error) {
+      // Show error toast message if update fails
+      toast.error("Could not update the profile detail");
+    }
+  }
+
+  // JSX rendering of the component
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -21,11 +67,34 @@ export default function Profile() {
         <div className="w-full md:w-[50%] mt-6 px-3">
           <form>
             {/* Name input */}
-            <input type="text" id='name' value={name} className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' disabled />
+            <input
+              type="text"
+              id='name'
+              disabled={!changeDetail}
+              onChange={onChange}
+              value={name}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border
+               border-gray-300 rounded transition ease-in-out ${changeDetail && "bg-red-200 focus:bg-red-200"}`}
+            />
             {/* Email input */}
-            <input type="email" id='email' value={email} className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out' disabled />
+            <input
+              type="email"
+              id='email'
+              value={email}
+              className=' mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out'
+              disabled
+            />
             <div className="flex justify-between whitespace-nowrap mb-6 text-sm md:text-lg">
-              <p className='flex items-center mb-6'>Do you want to change your name? <span className='text-red-600 hover:text-red-700 transition ease-in-out cursor-pointer ml-1 duration-200'>Edit</span></p>
+              <p className='flex items-center mb-6'>Do you want to change your name? <span className='text-red-600 hover:text-red-700 transition ease-in-out cursor-pointer ml-1 duration-200'
+                onClick={() => {
+                  // If in edit mode, submit the changes and toggle edit mode
+                  changeDetail && onSubmit();
+                  setChangeDetail((prevstate) => !prevstate);
+                }}>
+                {changeDetail ? "Apply change" : "Edit"}
+              </span>
+              </p>
+              {/* Button to sign out */}
               <p onClick={onLogout} className='text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 cursor-pointer'>Sign Out</p>
             </div>
           </form>
