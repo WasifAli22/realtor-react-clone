9667a10a5516bee7da21dba1ecc20c5a384d5483
Wasif Shahid <wasifaliwasif22@gmail.com>
2023-07-28 18:23:16 +0500

    Header component dynamically completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
+
 export default function Header() {
+    // State to manage the page state (e.g., "Sign In" or "Profile")
+    const [pageState, setPageState] = useState("Sign In")
+
+    // Get the Firebase auth instance
+    const auth = getAuth()
+
+    // Effect hook to update pageState when user authentication changes
+    useEffect(() => {
+        // Listen for authentication state changes
+        onAuthStateChanged(auth, (user) => {
+            // If the user is logged in, set pageState to "Profile"
+            // Otherwise, set it to "Sign In"
+            if (user) {
+                setPageState("Profile")
+            } else {
+                setPageState("Sign In")
+            }
+        })
+    }, [auth]) // Only run this effect when the auth instance changes
+
+    // Get the current location and navigation function from React Router
     const location = useLocation()
     const navigate = useNavigate()
-    function pathMathRoute(route) {
+
+    // Function to check if the given route matches the current location
+    function pathMatchRoute(route) {
         if (route === location.pathname) {
             return true
         }
+        // If the route doesn't match, return false
+        return false
     }
+
     return (
         <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
             <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
@@ -16,10 +44,13 @@ export default function Header() {
                 </div>
                 <div className="">
                     <ul className='flex space-x-10'>
-                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/') && "!text-black !border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
-                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/offers') && "!text-black !border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
-                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/sign-in') && "!text-black !border-b-red-500"}`} onClick={() => navigate("/sign-in")}>Sign In</li>
-
+                        {/* Home link with click event to navigate to the home page */}
+                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/') && "!text-black !border-b-red-500"}`} onClick={() => navigate("/")}>Home</li>
+                        {/* Offers link with click event to navigate to the offers page */}
+                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMatchRoute('/offers') && "!text-black !border-b-red-500"}`} onClick={() => navigate("/offers")}>Offers</li>
+                        {/* Profile/Sign In link with click event to navigate to the profile page if signed in, otherwise sign-in page */}
+                        <li className={`py-3 cursor-pointer text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
+                        ${(pathMatchRoute('/sign-in') || pathMatchRoute('/profile')) && "!text-black !border-b-red-500"}`} onClick={() => navigate("/profile")}>{pageState}</li>
                     </ul>
                 </div>
             </header>
